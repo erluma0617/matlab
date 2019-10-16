4220ac912f2d43c653ebed47a95dda1260b66bec
马二驴 <47710923+erluma0617@users.noreply.github.com>
2019-10-16 22:23:07 +0800

    Delete nest.m

--- nest.m
+++
@@ -1,7 +0,0 @@
-A=struct('data',[3 4 7;8 0 1],'nest', struct('testnum','Test 1','xdata',[4 2 8],'ydata',[7 1 6]));
-
-A(2).data=[9 3 2;7 6 5];
-A(2).nest.testnum='Test 2';
-A(2).nest.xdata=[3 4 2];
-A(2).nest,ydata=[5 0 9];
-A.nest
